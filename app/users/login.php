a950ff97aa6094235eef03298fa98f6cf13f25b5
NellySP <nelly.petren@gmail.com>
2021-12-13 10:22:56 +0100

    added structure

---
+++ app/users/login.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../autoload.php';
+
+
+// In this file we login users.
+
+if (isset($_POST['email'], $_POST['password'])) {
+    // We start by fetching the values from the $_POST array and save them to
+    // their own variables.
+    $email = trim($_POST['email']);
+
+
+    $query = 'SELECT * FROM users WHERE email = :email';
+
+    $statement = $database->prepare($query);
+
+    // Här sätter vi en placeholder för variabeln email. 
+    // Detta för att man inte får skicka in variabler i sql-queries. 
+
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+
+    $statement->execute();
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+    // if the user does not exist in the session, redirect to login-start-page
+
+    if ($user === false) {
+        redirect('/login.php');
+    }
+
+    // if (!$user) {
+    //     redirect('/login.php');
+    // } ovan går även att skriva så här
+
+    // if the user exist -> verify password
+
+    if (password_verify($_POST['password'], $user['password'])) {
+        // If the password was valid we know that the user exists and provided
+        // the correct password. We can now save the user in our session.
+        // Remember to not save the password in the session!
+        unset($user['password']);
+
+        $_SESSION['user'] = $user;
+    };
+}
+
+redirect('/');
---
+++ views/navigation.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/about.php">About</a>
+        </li>
+
+        <!-- if the user is set (logged in) don't show the log in button -->
+
+        <li class="nav-item">
+            <?php if (isset($_SESSION['user'])) : ?>
+                <a class="nav-link" href="/app/users/logout.php">Logout</a>
+            <?php else : ?>
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+            <?php endif; ?>
+        </li>
+    </ul>
+</nav>
