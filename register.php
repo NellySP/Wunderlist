1156eb4daf9a40a916882878f20db228acfbe2cd
NellySP <nelly.petren@gmail.com>
2021-12-17 10:48:08 +0100

    add labels

--- register.php
+++ register.php
@@ -5,9 +5,12 @@
     <h1>Registration</h1>
 
     <form class="form" action="" method="post">
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Username" required />
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="Email Adress">
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password">
         <input type="submit" name="submit" value="Register" class="login-button">
         <p class="link"><a href="login.php">Click to Login</a></p>
     </form>
