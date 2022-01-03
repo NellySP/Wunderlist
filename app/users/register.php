e9f799bbb49ba6159fbaed200d8511b005263311
NellySP <nelly.petren@gmail.com>
2022-01-03 15:50:41 +0100

    insert user to db

--- app/users/register.php
+++ app/users/register.php
@@ -8,7 +8,6 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
 
-// require('/app/database/db.php');
 // Logik:
 // 1: kolla om användare finns (email) om ja - echo "du är redan medlem!"
 // 2: om inte? Insert men först - >
@@ -22,40 +21,40 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $email = trim($_POST['email']);
     $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// chech if email exist in db
+    // check if email exist in db
 
-$statement = $database->prepare('SELECT email FROM Users WHERE email = :email');
-$statement->bindParam(':email', $email, PDO::PARAM_STR);
-$statement->execute();
-$checkEmail = $statement->fetch(PDO::FETCH_ASSOC);
-if ($checkEmail !== false) {
-    $_SESSION['errors'][] = "you already have an account!";
+    //prepare the statement
+    $statement = $database->prepare('SELECT email FROM Users WHERE email = :email');
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    //execute the statement
+    $statement->execute();
+    $checkEmail = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($checkEmail !== false) {
+        $_SESSION['errors'][] = "you already have an account!";
+    }
+
+    // $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    // $statement->bindParam(':user_name', $username, PDO::PARAM_STR);
+    // $statement->bindParam(':password', $password, PDO::PARAM_STR);
+
+
+    // Nu måste jag bara få den att lägga till infon
+
+
+
+    // insert query
+
+    $statement = $database->prepare('INSERT INTO Users
+(user_name, email, password)
+VALUES
+(:username, :email, :password)');
+
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+
+    $statement->execute();
+
+
+    redirect('/');
 }
-
-// $statement->bindParam(':email', $email, PDO::PARAM_STR);
-// $statement->bindParam(':user_name', $username, PDO::PARAM_STR);
-// $statement->bindParam(':password', $password, PDO::PARAM_STR);
-
-//email to check
-
-//prepare the statement
-$statement = $database->prepare("SELECT * FROM Users WHERE email=?");
-//execute the statement
-$statement->execute([$email]);
-//fetch result
-$user = $statement->fetch();
-if ($user) {
-    echo 'you already have an account!';
-};
-
-// Fram till hit fungerar det faktiskt!!!! inte nu längre
-
-// Nu måste jag bara få den att lägga till infon
-
-$sql = "INSERT INTO users (user_name, email, password) VALUES (:user_name,
-:email, :password)";
-
-// insert query
-
-
-// redirect('/');
