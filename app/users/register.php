82f8ac2179e0c0cc31c08fddd43ef33472e40215
NellySP <nelly.petren@gmail.com>
2022-01-03 16:53:30 +0100

    change placeholder

--- app/users/register.php
+++ app/users/register.php
@@ -8,22 +8,12 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
 
-// Logik:
-// 1: kolla om användare finns (email) om ja - echo "du är redan medlem!"
-// 2: om inte? Insert men först - >
-// 3: sanitera, validera, hasha och cutta - kolla hur man gör detta.
-// 4: om success - grattismeddelande
-// 5: om inte - något gick fel
-
 // values from input
 if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $username = trim($_POST['username']);
     $email = trim($_POST['email']);
     $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $statement->bindParam(':email', $email, PDO::PARAM_STR);
-    $statement->bindParam(':username', $username, PDO::PARAM_STR);
-    $statement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
     // check if email exist in db
 
     //prepare the statement
@@ -32,7 +22,7 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $statement->execute();
     $checkEmail = $statement->fetch(PDO::FETCH_ASSOC);
     if ($checkEmail !== false) {
-        echo "you already have an account!";
+        $_SESSION['errors'][] = "you already have an account!";
         redirect('/register.php');
     }
 
@@ -46,6 +36,10 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     (username, email, password)
     VALUES
     (:username, :email, :password)');
+
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
 
     $statement->execute();
 
