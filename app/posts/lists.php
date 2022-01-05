8ad1c3b40e6cb67492a6e1c47bf1bc1dec38ddd8
NellySP <nelly.petren@gmail.com>
2022-01-05 17:29:17 +0100

    draft of list entry to db

--- app/posts/lists.php
+++ app/posts/lists.php
@@ -6,14 +6,21 @@
 // fetch user_id from users where email like the one that is inloggad
 
 declare(strict_types=1);
-
 require __DIR__ . '/../autoload.php';
-$statement = $database->prepare('INSERT INTO Lists
-    (user_id, title)
-    VALUES
-    (:user_id, :title');
 
-$statement->bindParam(':title', $title, PDO::PARAM_STR);
-$statement->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+if (isset($_POST['submit'])) {
+    if (empty($_POST['title'])) {
+        $_SESSION['errors'][] = "Give your list a name!";
+    } else {
 
-$statement->execute();
+        $statement = $database->prepare('INSERT INTO Lists
+            (user_id, title)
+            VALUES
+            (:user_id, :title');
+
+        $statement->bindParam(':title', $title, PDO::PARAM_STR);
+        $statement->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+
+        $statement->execute();
+    }
+}
--- lists.php
+++ lists.php
@@ -7,7 +7,7 @@ require __DIR__ . '/views/header.php'; ?>
 
 <form method="post" action="posts/lists.php" class="input_form">
     <input type="text" name="list" class="list_input">
-    <button type="submit" name="submit" class="add_btn">Add list</button>
+    <button type="submit" name="submit" class="add_btn">Create list</button>
 </form>
 
 <!-- within the list, create new task -->
@@ -16,6 +16,12 @@ require __DIR__ . '/views/header.php'; ?>
     <input type="text" name="task" class="task_input">
     <button type="submit" name="submit" class="add_btn">Add Task</button>
 </form>
+
+<!-- Error message -->
+
+<?php if (isset($errors)) { ?>
+    <p><?php echo $errors; ?></p>
+<?php } ?>
 
 <!-- Overview of all current lists -->
 
