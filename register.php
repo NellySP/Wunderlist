2a6fdd695769803271242c32417984faac633f22
NellySP <nelly.petren@gmail.com>
2022-01-05 12:38:05 +0100

    add required input
    on registration form

--- register.php
+++ register.php
@@ -15,11 +15,11 @@
 
     <form action="/app/users/register.php" method="post">
         <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Username" required />
+        <input type="text" name="username" placeholder="Username" required>
         <label for="email">Email</label>
-        <input type="text" name="email" placeholder="Email Adress">
+        <input type="text" name="email" placeholder="Email Adress" required>
         <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Password">
+        <input type="password" name="password" placeholder="Password" required>
         <input id="register-button" type="submit" name="submit" value="Register" class="login-button">
         <p>Already have an account?</p>
         <p class="link"><a href="login.php">Click to Login</a></p>
