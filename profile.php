a2a068ebcc73a82604cec5acc8b8751981a44be3
NellySP <nelly.petren@gmail.com>
2022-01-06 10:50:14 +0100

    add form for updating email and password

--- profile.php
+++ profile.php
@@ -7,11 +7,6 @@ require __DIR__ . '/views/header.php'; ?>
         $name = $_SESSION['user']['name'];
         echo 'Welcome, ' . $name . '!';
     } ?></h2>
-<h2><?php if (isset($_SESSION['user'])) {
-        $user_id = $_SESSION['user']['user_id'];
-        echo 'Welcome, ' . $user_id . '!';
-    } ?></h2>
-
 
 <img src="" alt="profile picture">
 <!-- form to change profile picture -->
@@ -19,15 +14,23 @@ require __DIR__ . '/views/header.php'; ?>
 
 <!-- form to change email -->
 <p>Current email: <?= ($_SESSION['user']['email']); ?></p>
-<form action=""></form>
+<form action="/app/users/change-email.php">
+    <label for="email">Email</label>
+    <input class="form-control" type="email" name="email" id="email" placeholder="youremail@email.com" required>
+    <small class="form-text">Enter your new email adress.</small>
+</form>
 
 <!-- form to change password -->
-<form action=""></form>
+<form action="/app/users/change-password.php">
+    <label for="password">Password</label>
+    <input class="form-control" type="password" name="password" id="password" required>
+    <small class="form-text">Enter your new password.</small>
+</form>
 
 <!-- delete user -->
 <form action="/app/users/delete.php">
     <input type="hidden" name="userid-to-delete" value="<?php echo $_SESSION['user'] ?>">
-    <input type="submit" name="delete" value="delete">
+    <button type="submit" name="delete" value="delete">
 </form>
 <p>Click here to delete your user profile</p>
 
