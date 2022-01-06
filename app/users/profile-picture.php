3c84b4e4a9db214fae2ffd3cc9987f7239f23017
NellySP <nelly.petren@gmail.com>
2022-01-06 19:05:29 +0100

    add working image upload

--- app/users/profile-picture.php
+++ app/users/profile-picture.php
@@ -4,64 +4,35 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-// to upload an image for the first time
+// vad som ska göras: 
+// kolla om en bild satts i formuläret.
+// ge den rätt namn
+// kolla om bild uppfyller alla krav
+// kolla om bild finns i databas, om ja - update
+// om nej - insert. varför ska det vara så svårt????
+// bilden ska hamna i uploads-mappen. sökvägen i db?
+
+// check if image exists in form, if yes set a variable and specify filename
 
 if (isset($_FILES['avatar'])) {
-    $avatar = $_FILES['avatar'];
-    $user_id = $_SESSION['user']['user_id'];
+    $avatarFile = $_FILES['avatar'];
+    $avatarName = date('ymd') . '-' . $avatarFile['name'];
 
-    $avatar['name'] = date('Y-m-d') . $avatar['name'];
-    $direction = __DIR__ . '/uploads/' . $avatar['name'];
+    // Set path to where the file should upload
 
-    if ($avatar['type'] !== 'image/jpeg') {
-        $_SESSION['errors'][] = 'please choose an image in .jpg or .jpeg';
-        redirect('/profile.php');
-    } elseif ($avatar['size'] >= 2097152) {
-        $_SESSION['errors'][] = 'The image is too big! Please upload an image smaller than 2MB';
-        redirect('/profile.php');
-    } else {
-        move_uploaded_file($avatar['tmp_name'], $direction);
-    };
+    $path = __DIR__ . '/../../uploads/';
+    $destination = $path . $avatarName;
 
-    $avatarPath = '/app/users/uploads/' . $avatar['name'];
+    move_uploaded_file($avatarFile['tmp_name'], $destination);
 
-    $statement = $database->prepare('INSERT INTO Users (profile_picture) VALUES (:profile_picture) WHERE user_id = :user_id');
+    // insert the avatarname in to the db
 
-    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-    $statement->bindParam(':profile_picture', $avatarPath, PDO::PARAM_STR);
-
+    $statement  = $database->prepare('UPDATE users SET profile_picture = :avatar WHERE user_id = :user_id');
+    $statement->bindParam(':user_id', $_SESSION['user']['user_id'], PDO::PARAM_INT);
+    $statement->bindParam(':avatar', $avatarName, PDO::PARAM_STR);
     $statement->execute();
 
-    redirect('/profile.php');
-};
+    // $_SESSION['user']['avatar'] = $userAvatar;
+}
 
-//  to replace image (borde kunna gå att klämma in i tidigare logik? säkert en if-sats) 
-
-
-if (isset($_FILES['avatar'])) {
-    $avatar = $_FILES['avatar'];
-    $user_id = $_SESSION['user']['user_id'];
-
-    $avatar['name'] = date('Y-m-d') . $avatar['name'];
-    $direction = __DIR__ . 'uploads/' . $avatar['name'];
-
-    if ($avatar['type'] !== 'image/jpeg') {
-        $_SESSION['errors'][] = 'please choose an image in .jpg or .jpeg';
-        redirect('/profile.php');
-    } elseif ($avatar['size'] >= 2097152) {
-        $_SESSION['errors'][] = 'The image is too big! Please upload an image smaller than 2MB';
-        redirect('/profile.php');
-    } else {
-        move_uploaded_file($avatar['tmp_name'], $direction);
-    };
-
-    $avatarPath = '/app/users/uploads/' . $avatar['name'];
-
-    $statement  = $database->prepare('UPDATE users SET profile_picture = :profile_picture WHERE user_id = :user_id');
-    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-    $statement->bindParam(':profile_picture', $avatar, PDO::PARAM_STR);
-
-    $statement->execute();
-
-    redirect('/profile.php');
-};
+redirect('/profile.php');
