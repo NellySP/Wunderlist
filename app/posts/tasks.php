e133994d1dd880133581aa3120f584453a61076b
NellySP <nelly.petren@gmail.com>
2022-01-07 18:01:53 +0100

    add task insert logic
    not finished

--- app/posts/tasks.php
+++ app/posts/tasks.php
@@ -2,19 +2,27 @@
 
 // In this file we add lists to the database
 
-//  här måste jag hämta använadid/list-id. så nåt i stil med
-// fetch user_id from users where email like the one that is inloggad
-// fecth list_id where user_id like the one that is inloggad
+// hur placerar jag task under list?
 
 declare(strict_types=1);
 
-require __DIR__ . '/../autoload.php';
-$statement = $database->prepare('INSERT INTO Tasks
-    (list_id, title)
+// Create task
+
+if (isset($_POST['task'])) {
+    $task = trim(filter_var($_POST['task'], FILTER_SANITIZE_STRING));
+    $user_id = $_SESSION['user']['user_id'];
+
+    if (empty($_POST['title'])) {
+        $_SESSION['errors'][] = "Give your list a name!";
+    }
+    $statement = $database->prepare('INSERT INTO Lists
+    (user_id, list_id, title)
     VALUES
-    (:user_id, :title');
+    (:user_id, :list_id, :title)');
+    $statement->bindParam(':title', $task, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+    $statement->bindParam(':list_id', $list_id, PDO::PARAM_STR);
 
-$statement->bindParam(':title', $title, PDO::PARAM_STR);
-$statement->bindParam(':list_id', $user_id, PDO::PARAM_STR);
-
-$statement->execute();
+    $statement->execute();
+}
+redirect('/lists.php');
