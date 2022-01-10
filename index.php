cf6b132e318da84a1a7f1c0e9f1a8b4278000471
NellySP <nelly.petren@gmail.com>
2022-01-10 17:18:20 +0100

    add display tasks due today

--- index.php
+++ index.php
@@ -15,4 +15,22 @@ visa alla tasks due today.
 
 alternativ för att visa alla tasks?
 
+<h2>Tasks due today:</h2>
+
+<?php foreach (tasks_due_today($database) as $task) : ?>
+    <ul>
+        <li>
+            <h3><?= ($task['title']); ?></h3>
+        </li>
+    </ul>
+    <form action="completedtaskellernåt" method="POST">
+        <label for="completed"></label>
+        <input type="checkbox" name="checkbox" id="checkbox">
+    </form>
+    <form action="/app/posts/delete-task.php" method="post">
+        <input type="hidden" name="task" id="task" value="<?= $task['id'] ?>">
+        <button type="submit" class="delete">X</button>
+    </form>
+<?php endforeach; ?>
+
 <?php require __DIR__ . '/views/footer.php';
