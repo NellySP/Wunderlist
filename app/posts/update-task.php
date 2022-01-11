8095ce00244a622529aae874c0cb9fe32208eb85
NellySP <nelly.petren@gmail.com>
2022-01-11 20:18:21 +0100

    add new update logic to tasks
    still only updates title

--- app/posts/update-task.php
+++ app/posts/update-task.php
@@ -6,58 +6,66 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we update tasks
 
-
 $list_id = $_POST['list-id'];
 $user_id = $_SESSION['user']['user_id'];
+$id = ($_POST['task-id']);
 
-// update title
+// check if title is set, if yes -> set variable
 
 if (isset($_POST['task'])) {
     $title = trim($_POST['task']);
-    $description = ($_POST['description']);
-    $deadline = ($_POST['deadline']);
-    $id = ($_POST['task-id']);
-
-
-    $statement = $database->prepare('UPDATE Tasks SET title = :title WHERE id = :id');
-
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->bindParam(':title', $title, PDO::PARAM_STR);
-
-    $statement->execute();
-    back();
 }
 
-// update description
+// description
 
 if (isset($_POST['desciption'])) {
     $description = trim($_POST['description']);
-    $id = ($_POST['task-id']);
-
-
-    $statement = $database->prepare('UPDATE Tasks SET description = :description WHERE id = :id');
-
-    $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->bindParam(':description', $description, PDO::PARAM_STR);
-
-    $statement->execute();
-
-    back();
 }
-
-// update deadline
+// deadline
 
 if (isset($_POST['deadline'])) {
     $deadline = ($_POST['deadline']);
-    $id = ($_POST['task-id']);
+}
 
+//check which attributes have been edited, then update db
+if ($title) {
+    $statement = $database->prepare(
+        'UPDATE Tasks SET title = :title
+    WHERE user_id = :user_id AND list_id = :list_id AND id = :id'
+    );
 
-    $statement = $database->prepare('UPDATE Tasks SET deadline = :deadline WHERE id = :id');
-
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':list_id', $list_id, PDO::PARAM_INT);
     $statement->bindParam(':id', $id, PDO::PARAM_INT);
-    $statement->bindParam(':deadline', $deadline, PDO::PARAM_STR);
-
     $statement->execute();
 
-    back();
+    if ($description) {
+        $statement = $database->prepare(
+            'UPDATE Tasks
+    SET description = :description
+    WHERE user_id = :user_id AND list_id = :list_id AND id = :task_id'
+        );
+
+        $statement->bindParam(':description', $description, PDO::PARAM_STR);
+        $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $statement->bindParam(':list_id', $list_id, PDO::PARAM_INT);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    if ($deadline) {
+        $statement = $database->prepare(
+            'UPDATE Tasks
+    SET deadline = :deadline
+    WHERE user_id = :user_id AND list_id = :list_id AND id = :id'
+        );
+
+        $statement->bindParam(':deadline', $deadline, PDO::PARAM_STR);
+        $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $statement->bindParam(':list_id', $list_id, PDO::PARAM_INT);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
+back();
