c2f16cb3d4bcaef6223d33f9843a18d0e042907c
DavisDavisDavis <olizoooom@gmail.com>
2022-01-26 09:49:49 +0100

    welcome email on registration ✨
    By using PHP mailer from github and the website mailtrap! 📀

--- app/functions.php
+++ app/functions.php
@@ -2,6 +2,45 @@
 
 declare(strict_types=1);
 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+function send_mail($email_recipient)
+{
+    $mail = new PHPMailer();
+
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '41ddeff6cad56a';
+        $mail->Password = '57da91fa1bbaa4';
+        //Recipients
+        $mail->setFrom('wunderlist@example.com', 'Mailer');
+        $mail->addAddress($email_recipient);     //Add a recipient
+        $mail->addReplyTo('wunderlist@example.com', 'support');
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Welcome to wunderlist <3';
+        $mail->Body    = '<img src="https://c.tenor.com/E33HkUhvr9EAAAAC/welcome.gif" alt="">';
+        $mail->AltBody = 'Welcome to Wunderlist <3';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
 function redirect(string $path)
 {
     header("Location: ${path}");
@@ -150,18 +189,5 @@ function task_status(PDO $database, $id)
         } else if ($state == false) {
             echo " not completed ❌";
         }
-    }
-}
-
-
-function complete_all(PDO $database)
-{
-    $query = 'SELECT completed FROM Tasks';
-
-    $statement = $database->query($query);
-    $status = $statement->fetchALl(PDO::FETCH_ASSOC);
-
-    foreach ($status as $state) {
-        echo $state;
     }
 }
